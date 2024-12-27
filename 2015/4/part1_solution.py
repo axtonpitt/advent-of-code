8b08ea734adbf850bfba91db1e9d965ee3b496d0
Axton Pitt <axton@litmaps.co>
2024-12-28 10:52:34 +1300

    Solve 2015 day 4 part 1
    Signed-off-by: Axton Pitt <axton@litmaps.co>

---
+++ 2015/4/part1_solution.py
@@ -0,0 +1,34 @@
+import argparse
+import hashlib
+
+parser = argparse.ArgumentParser(description="Solution to 2015 Day 4 – Part 1")
+parser.add_argument("-i", "--input-file", help="Path to input file (with \n seperated data)")
+parser.add_argument("-t", "--target-sum", help="Target sum")
+args = parser.parse_args()
+
+secret_key = ''
+
+with open(args.input_file, mode="r") as reader:
+    for line in reader:
+        secret_key = line
+
+hash_increment = 10000000000000
+hash_input = 0
+
+for i in range(0, hash_increment):
+    hash_input += 1
+    m = hashlib.md5()
+    m.update(secret_key.encode())
+    m.update(str(hash_input).encode())
+    result = m.hexdigest()
+    if result[:5] == "00000":
+        print(result)
+        break
+
+if args.target_sum != None:
+    if int(args.target_sum) == hash_input:
+        print('Target matches output')
+    else:
+        print('Output is incorrect')
+else:
+    print(hash_input)
