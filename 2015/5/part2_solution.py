fe2a29dac1bd4a0144a4c5d82347d58b76cef167
Axton Pitt <axton@litmaps.co>
2025-01-03 16:38:39 +1300

    WIP

--- 2015/5/part2_solution.py
+++ 2015/5/part2_solution.py
@@ -15,8 +15,6 @@ with open(args.input_file, mode="r") as reader:
 nice_strings = []
 
 for string in strings:
-    prev_char = ''
-    prev_prev_char = ''
     two_letter_combos = []
     contains_two_letter_repeat = False
 
@@ -24,17 +22,21 @@ for string in strings:
     contains_two_letter_repeat_with_gap = False
 
     for i in range(0, len(string)):
-        char = string[i]
-        if prev_char != '':
-            two_letter_key = f'{prev_char}{char}'
-            two_letter_pairs[two_letter_key] = two_letter_pairs.get(two_letter_key, 0) + 1
+        if (i - 1) >= 0:
+            two_char = string[i-1:i]
+            two_letter_combos.append(two_char)
 
-        if prev_prev_char != '' and prev_char != '':
-            if prev_prev_char == char:
-                contains_letter_repeat = True
+            prev_two_char_array_index = i - 2
 
-        prev_prev_char = prev_char
-        prev_char = char
+            if two_letter_combos.get(prev_two_char_array_index, ''):
+
+
+        if (i - 2) >= 0:
+            three_char = string[i-2:i]
+            three_letter_combos.append(three_char)
+
+            prev_three_char_array_index = i - 3
+
     
     for pair, freq in two_letter_pairs.items():
         if freq > 1:
